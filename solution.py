36289f3582422286eebf12083e363a2b34da8654
Inga-DS <80358736+Inga-DS@users.noreply.github.com>
2023-04-14 19:02:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,16 @@ import numpy as np
 chat_id = 285728981 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean(0.99) # Ваш ответ
+    from scipy import stats
+from statsmodels.stats.proportion import proportions_ztest
+
+
+def solution(x_success: int, 
+             x_cnt: int, 
+             y_success: int, 
+             y_cnt: int) -> bool:
+    
+    alpha = 0.008
+    stats, p_value = proportions_ztest(np.array([x_success, y_success]), np.array([x_cnt, y_cnt]), alternative = 'smaller')
+    
+    return True if p_value < alpha else False
